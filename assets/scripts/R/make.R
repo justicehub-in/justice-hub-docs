45f8dd330a8ecc8b183792d833121775af4591ec
apoorv <apoorv7491@gmail.com>
2020-03-08 18:49:53 +0530

    Add push command

--- assets/scripts/R/make.R
+++ assets/scripts/R/make.R
@@ -2,3 +2,9 @@ source("assets/scripts/R/packages.R")
 source("assets/scripts/R/functions.R")
 
 update_website()
+
+cred <- git2r::cred_token()
+# git2r::status()
+# git2r::add(path = ".")
+# git2r::commit(message = glue("render dashboard - {Sys.Date()}"))
+git2r::push(object = ".", name = 'origin',refspec = "refs/heads/master",credentials = cred)
